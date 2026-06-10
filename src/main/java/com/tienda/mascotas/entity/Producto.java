d23589a2172fba9e89ba4655855750703e9c400f
jonnycanabal <jonny.canabal@gmail.com>
2026-06-10 17:58:53 -0500

    Crear entidad Producto y configurar PostgreSQL

---
+++ src/main/java/com/tienda/mascotas/entity/Producto.java
@@ -0,0 +1,32 @@
+package com.tienda.mascotas.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "producto")
+@Getter
+@Setter
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private Double precio;
+
+    private String marca;
+
+    private Integer stock;
+
+    public Producto() {
+
+    }
+}

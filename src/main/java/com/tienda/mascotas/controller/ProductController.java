bba784924d4c0c01c324593bdfd52b7e395f3353
jonnycanabal <jonny.canabal@gmail.com>
2026-06-12 19:45:33 -0500

    Implementación busca de productos por id

--- src/main/java/com/tienda/mascotas/controller/ProductController.java
+++ src/main/java/com/tienda/mascotas/controller/ProductController.java
@@ -2,9 +2,12 @@ package com.tienda.mascotas.controller;
 
 import com.tienda.mascotas.entity.Product;
 import com.tienda.mascotas.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Controlador REST encargado de recibir las peticiones relacionadas
@@ -28,6 +31,19 @@ public class ProductController {
         return productService.getAllProducts();
     }
 
+    /*** Busca un producto por su ID. Retorna el producto si existe o un mensaje de error 404 si no es encontrado.*/
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProductById(@PathVariable Long id){
+        Optional<Product> product = productService.getProductById(id);
+
+        if(product.isEmpty()){
+            return ResponseEntity.status(404).body("Producto no encontrado");
+        }
+
+        return ResponseEntity.ok(product.get());
+    }
+
+    /** Crea un nuevo producto*/
     @PostMapping
     public Product createProduct(@RequestBody Product product) {
         return productService.createProduct(product);
--- src/main/java/com/tienda/mascotas/service/ProductService.java
+++ src/main/java/com/tienda/mascotas/service/ProductService.java
@@ -3,8 +3,10 @@ package com.tienda.mascotas.service;
 import com.tienda.mascotas.entity.Product;
 import com.tienda.mascotas.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Contiene la lógica de negocio relacionada con los productos.
@@ -23,7 +25,15 @@ public class ProductService {
         return productRepository.findAll();
     }
 
+    /** Obtiene el producto según su ID*/
+    public Optional<Product> getProductById(Long id){
+        return productRepository.findById(id);
+    }
+
+    /** Guarda un nuevo producto*/
     public Product createProduct(Product product) {
         return productRepository.save(product);
     }
+
+
 }
